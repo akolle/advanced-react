047939b658152c2e51a460f636b800a8db43f9f5
akolle <andykoller55@yahoo.com>
2024-08-06 18:50:40 -0400

    optional chaining when certain data is missing

--- src/tutorial/05-leverage-javascript/starter/Person.jsx
+++ src/tutorial/05-leverage-javascript/starter/Person.jsx
@@ -1,6 +1,10 @@
+import defaultImg from '../../../assets/default-avatar.svg'
 import React from 'react'
-export function Person({ name, nickName, images }) {
-  const img = images[0].small.url
+export function Person({ name, nickName = 'shakAndBake', images }) {
+  // const img = images && images[0] && images[0].small && images[0].small.url;
+  // const img = images?.[0]?.small?.url || defaultImg //optional chaining
+  const img = images?.[0]?.small?.url ?? defaultImg //optional chaining with ?? instead of or (||)
+
   return (
     <div>
       <img src={img} alt="{name}" style={{ width: '50px' }} />
