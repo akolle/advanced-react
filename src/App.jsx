e51f80d4cad00dbe651afaddadfb031e64c05371
akolle <andykoller55@yahoo.com>
2024-08-06 18:37:58 -0400

    leaveraging javascript example

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import Starter from './tutorial/02-useEffect/starter/05-cleanup-function';
+import List from './tutorial/05-leverage-javascript/starter/List'
 
 function App() {
   return (
-    <div className='container'>
-      <Starter />
+    <div className="container">
+      <List />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
