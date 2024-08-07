29a31fb4bdd5cf2a5de029c0067bb8b238378213
akolle <andykoller55@yahoo.com>
2024-08-06 21:17:13 -0400

    adding user logic setup

--- src/tutorial/06-forms/starter/02-user-challenge.jsx
+++ src/tutorial/06-forms/starter/02-user-challenge.jsx
@@ -1,21 +1,39 @@
+import { useState } from 'react'
+import { data } from '../../../data/'
+
 const UserChallenge = () => {
+  const [name, setName] = useState('')
+  const [newData, setNewData] = useState(data)
+
+  const handleSubmit = (e) => {
+    //do something
+    e.preventDefault()
+    console.log(name)
+  }
+
   return (
     <div>
-      <form className='form'>
+      <form className="form">
         <h4>Add User</h4>
-        <div className='form-row'>
-          <label htmlFor='name' className='form-label'>
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
             name
           </label>
-          <input type='text' className='form-input' id='name' />
+          <input
+            type="text"
+            className="form-input"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
         </div>
 
-        <button type='submit' className='btn btn-block'>
+        <button type="submit" className="btn btn-block">
           submit
         </button>
       </form>
       {/* render users below */}
     </div>
-  );
-};
-export default UserChallenge;
+  )
+}
+export default UserChallenge
