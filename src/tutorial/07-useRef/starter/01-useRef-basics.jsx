16dbb805f0b4b48cff33950c6c90cba72cc4b64b
akolle <andykoller55@yahoo.com>
2024-08-09 17:00:16 -0400

    useRef with initial rendering

--- src/tutorial/07-useRef/starter/01-useRef-basics.jsx
+++ src/tutorial/07-useRef/starter/01-useRef-basics.jsx
@@ -3,12 +3,13 @@ import { useEffect, useRef, useState } from 'react'
 const UseRefBasics = () => {
   const [value, setValue] = useState(0)
   const refContainer = useRef(null)
+  const isMounted = useRef(false)
 
   // console.log(refContainer);
 
-  // useEffect(()=>{
-  //   console.log(refContainer);
-  // })
+  useEffect(() => {
+    refContainer.current.focus()
+  })
 
   const handleSubmit = (e) => {
     e.preventDefault()
