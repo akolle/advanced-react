4531b4f74a0e02a5a278a21c0054943f8985404c
akolle <andykoller55@yahoo.com>
2024-08-09 16:12:30 -0400

    testing which git repo im in

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import List from './tutorial/06-forms/starter/05-form-data'
 function App() {
   return (
     <div className="container">
-      <List />
+      {/* <List /> */}
+      test
     </div>
   )
 }
